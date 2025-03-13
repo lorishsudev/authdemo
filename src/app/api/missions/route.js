6e84ffcc75438d03408bab52709d224b373ff9f6
Loris Hsu 許銘欽 <loris.hsu@lcygroup.com>
2025-03-13 22:05:52 +0800

    20250313 V01

--- src/app/api/missions/route.js
+++ src/app/api/missions/route.js
@@ -3,8 +3,24 @@ import { NextResponse } from 'next/server';
 
 
 export const GET = async () => {
-  const missions = await prisma.Missions.findMany();
+  const missions = await prisma.missions.findMany();
 
   return NextResponse.json(missions);
 
 };
+
+export const POST = async (request) => {
+  try {
+    const body = await request.json();
+
+    const newMissions = await prisma.missions.create({
+        data: body  // 直接使用 body，而不是解構
+    });
+
+    return NextResponse.json(newMissions);
+
+} catch (err) {
+    return NextResponse.json({ message: "POST Error", err }, { status: 500 });
+}
+}
+
